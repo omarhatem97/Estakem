508b67033e44c18a290418d0b6b3013d329de16c
sohayb <sohaybbahi@gmail.com>
2021-05-08 11:42:22 +0200

    Data Classes Fix

--- app/src/main/java/com/example/islamics/estakem/ui/data/HabitsData.kt
+++ app/src/main/java/com/example/islamics/estakem/ui/data/HabitsData.kt
@@ -1,6 +1,4 @@
 package com.example.islamics.estakem.ui.data
 
-import android.widget.TextView
-
 data class HabitsData (var name : String, var thickbar:Int, var thinbar:Int, var coinimg:Int,
                        var numofcoins:String, var numdays:String, var rating:Float, var check:Boolean, var DayWeek:String)
